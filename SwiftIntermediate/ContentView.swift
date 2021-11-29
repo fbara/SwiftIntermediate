4417946c5fcfa5ad9f397cc649b58f1d2f8dcbcf
fbara <fbara1@yahoo.com>
2021-11-29 14:31:31 -0600

    Initial Commit

---
+++ SwiftIntermediate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftIntermediate
+//
+//  Created by Frank Bara on 11/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftIntermediate/SwiftIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftIntermediateApp.swift
+//  SwiftIntermediate
+//
+//  Created by Frank Bara on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
