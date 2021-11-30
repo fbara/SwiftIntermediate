314e08baa62737858c4183aa35220ba3a4d0ffab
fbara <fbara1@yahoo.com>
2021-11-30 11:28:57 -0600

    Final commit for Long Press gesture.

---
+++ SwiftIntermediate/LongPressGestureBootcamp.swift
@@ -0,0 +1,87 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftIntermediate
+//
+//  Created by Frank Bara on 11/30/21.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("Click Here")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+                        // at min duration
+                        withAnimation(.easeInOut) {
+                            isSuccess = true
+                        }
+                    } onPressingChanged: { isPressing in
+                        // start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeInOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                    }
+            }
+            
+            
+                Text("Reset")
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+                
+                
+            }
+            
+        }
+        
+//        Text(isComplete ? "Completed" : "Not complete")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+////            .onTapGesture {
+////                isComplete.toggle()
+////            }
+//            .onLongPressGesture(minimumDuration: 1, maximumDistance: 1) {
+//                isComplete.toggle()
+//            }
+    }
+}
+
+struct LongPressGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGestureBootcamp()
+    }
+}
