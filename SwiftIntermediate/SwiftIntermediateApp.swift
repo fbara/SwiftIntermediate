3074b68fe8d5f99903a2a893f5ccd8fc212a9611
fbara <fbara1@yahoo.com>
2021-12-01 10:45:59 -0600

    Final commit for Magnification Gesture.

--- SwiftIntermediate/SwiftIntermediateApp.swift
+++ SwiftIntermediate/SwiftIntermediateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftIntermediateApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MagnificationGestureBootcamp()
         }
     }
 }
