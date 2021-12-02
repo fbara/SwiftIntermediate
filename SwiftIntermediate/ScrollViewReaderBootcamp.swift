b9f6332cdc0042a5a4d97b68a9689eb2bd68412f
fbara <fbara1@yahoo.com>
2021-12-02 09:37:47 -0600

    Final commit for ScrollView and ScrollViewReader.

---
+++ SwiftIntermediate/ScrollViewReaderBootcamp.swift
@@ -0,0 +1,63 @@
+//
+//  ScrollViewReaderBootcamp.swift
+//  SwiftIntermediate
+//
+//  Created by Frank Bara on 12/2/21.
+//
+
+import SwiftUI
+
+struct ScrollViewReaderBootcamp: View {
+    
+    @State var textFieldText: String = ""
+    @State var scrollToIndex: Int = 0
+    
+    var body: some View {
+        
+        VStack {
+            
+            TextField("Enter a number here...", text: $textFieldText)
+                .frame(height: 55)
+                .border(Color.gray)
+                .padding(.horizontal)
+                .keyboardType(.numberPad)
+            
+            Button("Scroll now") {
+                withAnimation(.spring()) {
+                    if let index = Int(textFieldText) {
+                        scrollToIndex = index
+                    }
+                }
+            }
+            
+            ScrollView {
+                ScrollViewReader { proxy in
+                                        
+                    ForEach(0..<50) { index in
+                        Text("This is item #\(index).")
+                            .font(.headline)
+                            .frame(height: 200)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .padding()
+                            .id(index)
+                        
+                    }
+                    .onChange(of: scrollToIndex, perform: { value in
+                        withAnimation(.spring()) {
+                            proxy.scrollTo(value, anchor: nil)
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewReaderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReaderBootcamp()
+    }
+}
